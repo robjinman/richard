3f52a0bcfff598c1534010589da47b91b5d7f60b
Rob Jinman <jinmanr@gmail.com>
2023-08-27 19:02:31 +0100

    imageprep can resize images

--- tools/imageprep/src/main.cpp
+++ tools/imageprep/src/main.cpp
@@ -2,20 +2,23 @@
 #include <cstdlib>
 #include <jpeglib.h>
 #include <iostream>
+#include <filesystem>
 #include "bitmap.hpp"
 
 using namespace cpputils;
 
-void read_JPEG_file(const char *filename) {
+namespace {
+
+Bitmap readJpegFile(const std::string& filename) {
   struct jpeg_decompress_struct cinfo;
   struct jpeg_error_mgr jerr;
   JSAMPARRAY buffer;
-  int row_stride;
+  int rowStride;
 
   cinfo.err = jpeg_std_error(&jerr);
   jpeg_create_decompress(&cinfo);
 
-  FILE *infile = fopen(filename, "rb");
+  FILE* infile = fopen(filename.c_str(), "rb");
   if (infile == nullptr) {
     std::cerr << "Error opening file: " << filename << std::endl;
     exit(1);
@@ -25,8 +28,8 @@ void read_JPEG_file(const char *filename) {
   jpeg_read_header(&cinfo, TRUE);
 
   jpeg_start_decompress(&cinfo);
-  row_stride = cinfo.output_width * cinfo.output_components;
-  buffer = (*cinfo.mem->alloc_sarray)((j_common_ptr) &cinfo, JPOOL_IMAGE, row_stride, 1);
+  rowStride = cinfo.output_width * cinfo.output_components;
+  buffer = (*cinfo.mem->alloc_sarray)((j_common_ptr) &cinfo, JPOOL_IMAGE, rowStride, 1);
 
   size_t size[] = { cinfo.output_height, cinfo.output_width, cinfo.output_components };
   Bitmap bm(size);
@@ -35,7 +38,7 @@ void read_JPEG_file(const char *filename) {
   while (cinfo.output_scanline < cinfo.output_height) {
     jpeg_read_scanlines(&cinfo, buffer, 1);
 
-    for (int i = 0; i < row_stride; i += 3) {
+    for (int i = 0; i < rowStride; i += 3) {
       bm[cinfo.output_height - 1 - j][i / 3][2] = buffer[0][i];
       bm[cinfo.output_height - 1 - j][i / 3][1] = buffer[0][i + 1];
       bm[cinfo.output_height - 1 - j][i / 3][0] = buffer[0][i + 2];
@@ -48,14 +51,56 @@ void read_JPEG_file(const char *filename) {
   jpeg_destroy_decompress(&cinfo);
   fclose(infile);
 
-  saveBitmap(bm, "out.bmp");
+  return bm;
+}
+
+Bitmap processImage(const Bitmap& srcImage) {
+  const size_t W = 100;
+  const size_t H = 100;
+
+  size_t size[] = { W, H, 3 };
+  Bitmap finalImage(size);
+
+  for (size_t j = 0; j < H; ++j) {
+    for (size_t i = 0; i < W; ++i) {
+      double y = static_cast<double>(j) / H * srcImage.size()[0];
+      double x = static_cast<double>(i) / W * srcImage.size()[1];
+
+      size_t j_ = static_cast<size_t>(y);
+      size_t i_ = static_cast<size_t>(x);
+
+      finalImage[j][i][0] = srcImage[j_][i_][0];
+      finalImage[j][i][1] = srcImage[j_][i_][1];
+      finalImage[j][i][2] = srcImage[j_][i_][2];
+    }
+  }
+
+  return finalImage;
+}
+
 }
 
 int main(int argc, char **argv) {
-  if (argc != 2) {
-    std::cerr << "Usage: " << argv[0] << " <JPEG file path>" << std::endl;
+  if (argc != 3) {
+    std::cerr << "Usage: " << argv[0] << " input_dir output_dir" << std::endl;
     return 1;
   }
-  read_JPEG_file(argv[1]);
+
+  std::string inputDir(argv[1]);
+  std::filesystem::path outputDir(argv[2]);
+
+  std::filesystem::create_directories(outputDir);
+
+  for (const auto& dirEntry : std::filesystem::directory_iterator{inputDir}) {
+    if (std::filesystem::is_regular_file(dirEntry)) {
+      std::cout << "Processing file '" << dirEntry.path() << "'..." << std::endl;
+
+      Bitmap srcImage = readJpegFile(dirEntry.path());
+      Bitmap finalImage = processImage(srcImage);
+
+      saveBitmap(finalImage, outputDir/dirEntry.path().stem().concat(".bmp"));
+    }
+  }
+
   return 0;
 }
